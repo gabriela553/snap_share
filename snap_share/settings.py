8f75428d10f9bef6f3f73b2befaa3fad92a31cbf
Gabi <gabi007gaj@gmail.com>
2024-09-21 21:50:36 +0200

    db_changed_to_pg

--- snap_share/settings.py
+++ snap_share/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = "snap_share.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'snap-share',
+        'USER': 'postgres',
+        'PASSWORD': 'Alpaka77',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
