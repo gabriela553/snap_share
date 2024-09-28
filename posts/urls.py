355615e1ccaba3541a608beeef31d3dc2f49bcec
Gabi <gabi007gaj@gmail.com>
2024-09-28 16:23:12 +0200

    logowanie_modelviewset_refractor

--- posts/urls.py
+++ posts/urls.py
@@ -4,16 +4,14 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     CommentCreateView,
     LikeCreateView,
-    PostCreateView,
     PostViewSet,
 )
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet)
+router.register(r"comments", CommentCreateView)
+router.register(r"likes", LikeCreateView)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("add/", PostCreateView.as_view(), name="add_post"),
-    path("comments/", CommentCreateView.as_view(), name="create_comment"),
-    path("likes/", LikeCreateView.as_view(), name="like_create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,45 +2,41 @@ from rest_framework import status, viewsets
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from .models import Like, Post
+from .models import Comment, Like, Post
 from .serializers import CommentSerializer, LikeSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
+    queryset = (
+        Post.objects.all()
+        .order_by("-created_at")
+        .select_related("author")
+        .prefetch_related("likes", "comments")
+    )
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
-class PostCreateView(APIView):
-    permission_classes = [IsAuthenticated]
-    parser_classes = (MultiPartParser, FormParser)
-
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CommentCreateView(APIView):
+class CommentCreateView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
-class LikeCreateView(APIView):
+class LikeCreateView(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_clas = LikeSerializer
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def create(self, request):
         post_id = request.data.get("post")
         if not post_id:
             return Response(
